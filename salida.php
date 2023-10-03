8818c5af7eddb68867505cbb69bc11a78c29f893
jhohanes95 <58569427+jhohanes95@users.noreply.github.com>
2023-10-03 16:02:17 -0500

    Update salida.php

--- salida.php
+++ salida.php
@@ -110,7 +110,7 @@ $_SESSION['variable'] = $miVariable;
                     print_r($salida);
                 }
             } else {
-                echo "No se encontró el valor de 'estado' en la salida JSON.<br>php";
+                echo "No se encontró el valor de 'estado' en la salida JSON php.<br>php";
             }
 
             echo "</pre>";
